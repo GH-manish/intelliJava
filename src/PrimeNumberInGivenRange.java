415873a900f775a6e1d1adf95610ce8f59af7dce
GH-manish <164605085+GH-manish@users.noreply.github.com>
2024-06-03 01:16:03 +0530

    Update PrimeNumberInGivenRange.java

--- src/PrimeNumberInGivenRange.java
+++ src/PrimeNumberInGivenRange.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class PrimeNumberInGivenRange {
     public static void main(String[] args) {
-        
+        int x,y,i,j,count=0;
+        System.out.print("enter your starting range  to find prine no. :");
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        System.out.print("enter your last range to find prime no. :");
+        y=sc.nextInt();
+        for (i=x;i<=y;i++)
+        {
+            for (j=2;j<i;j++)
+             {
+                if (i%j==0)
+                    break;
+             }
+            if (j==i)
+                count++;
+        }
+        System.out.println("total no. of prime no. is "+count);
     }
 }
