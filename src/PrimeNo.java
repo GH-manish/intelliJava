2646cc99490085fee908367ae434c3aac5aa1911
GH-manish <164605085+GH-manish@users.noreply.github.com>
2024-06-03 00:37:14 +0530

    prime no

--- src/PrimeNo.java
+++ src/PrimeNo.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class PrimeNo {
+
+    public static void main(String[] args) {
+        System.out.println("enter any no. : ");
+        Scanner sc=new Scanner(System.in);
+        int i,n=sc.nextInt();
+        for( i=2;i<n/2;i++)
+            if(n%i==0)
+                break;
+        if (i==n/2)
+            System.out.println("prime");
+        else
+            System.out.println("not prime");
+    }
+
 }
