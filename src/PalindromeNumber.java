4733ff4523f1bb14466440ee21bbd16e1effc02a
GH-manish <164605085+GH-manish@users.noreply.github.com>
2024-06-04 19:07:15 +0530

    Update PalindromeNumber.java

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class PalindromeNumber {
     public static void main(String[] args) {
-
+        int n,n1,y=0,r;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter any no. to check palindrome = ");
+                n= sc.nextInt();
+                n1=n;
+                while (n1>0)
+                {
+                  r=n1%10;
+                  n1=n1/10;
+                  y=y*10+r;
+                }
+                if (n==y)
+                {
+                    System.out.println("entered no. is palindrome ");
+                }
+                else
+                    System.out.println("entered no is not palindrome");
     }
 }
